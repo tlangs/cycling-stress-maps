26349f1df7e950a09c0d7af5cd17a052db51bf1f
Trevyn Langsford <tlangsford@gmail.com>
2026-06-15 20:19:02 -0400

    wu-turn start

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import ProposedRoutesMap from "./routes/ProposedRoutesMap/ProposedRoutesMap";
 import GoBostonLevelOfSressMap from "./routes/GoBoston/GoBostonLevelOfStressMap";
 import DefaultMap from "./routes/DefaultMap/DefaultMap";
 import NetworkMap from "./routes/NetworkMap/NetworkMap.tsx";
+import WuTurnMap from "./routes/WuTurn/WuTurnMap.tsx";
 
 // REGISTER ERROR OVERLAY
 const showErrorOverlay = (err: ErrorEvent) => {
@@ -33,6 +34,7 @@ createRoot(document.getElementById("root")!).render(
         <Route path="/network" element={<NetworkMap />} />
         <Route path="/proposed-routes" element={<ProposedRoutesMap />} />
         <Route path="/go-boston" element={<GoBostonLevelOfSressMap />} />
+        <Route path="/wu-turn" element={<WuTurnMap />} />
         {/* <Route path="/stress-map" element={<LevelOfStressRouteMap/>}/>
         <Route path="/go-boston" element={<GoBostonLevelOfStressMap/>}/>
         <Route path="/bcu-map" element={<BostonCyclistsUnionMap/>}/> */}
